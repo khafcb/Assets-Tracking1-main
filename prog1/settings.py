adc393ce88ba2369717892626beccfdabcc60cfd
khafcb <62855136+khafcb@users.noreply.github.com>
2021-04-24 03:00:14 +0300

    Update settings.py

--- prog1/settings.py
+++ prog1/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'assetstracking'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
