19888d50156585679937c261f4691efecb780f8d
khafcb <62855136+khafcb@users.noreply.github.com>
2021-04-24 23:21:32 +0300

    Update forms.py

--- assetstracking/forms.py
+++ assetstracking/forms.py
@@ -1,12 +1,13 @@
 from django.forms import ModelForm
-from .models import Borrowing, Tag
+from .models import *
 
 
 class BorrowingForm(ModelForm):
     class Meta:
         model = Borrowing
-        fields = ['end_date', 'employee_id', 'tag_id']
-        
+        fields=['end_date', 'tag_id', 'subscriber_id']
+
+
 class BorrowingFormEmployee(ModelForm):
     class Meta:
         model = Borrowing
@@ -16,4 +17,10 @@ class BorrowingFormEmployee(ModelForm):
 class AssetForm(ModelForm):
     class Meta:
         model = Tag
-        fields = ['tag_id', 'asset_name', 'rfid_id', 'asset_status', 'asset_location']
+        fields=['tag_id', 'asset_name', 'rfid_id', 'asset_status', 'asset_location']
+
+
+class extendingForm(ModelForm):
+    class Meta:
+        model = Borrowing
+        fields = ['end_date']
